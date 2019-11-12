41e8ebdf6d028de7f95b0685e2e52abf280139f7
tkyc@arch <terrykyc.7000@gmail.com>
2019-11-11 18:49:51 -0800

    posting function

--- route/authRouter.js
+++ route/authRouter.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const authController = require("../controller/authController")
+const authController = require("../controller/authController");
+const postController = require("../controller/postController");
 
 // Login routes
-router.get("/", authController.index)
-router.post("/login", authController.login)
+router.get("/", authController.index);
+router.post("/login", authController.login);
 
 // Register routes
-router.post("/register",authController.register)
+router.post("/register", authController.register);
 
+// Posting routes
+router.post("/post", postController.post);
 module.exports = router;
